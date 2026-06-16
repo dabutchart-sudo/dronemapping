68c04c9ef0ca54151a5088c53f17e5af1be25959
dabutchart-sudo <dabutchart@googlemail.com>
2026-06-16 19:48:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+// main.js
+import { initMap, redrawMap3D } from './mapEngine.js';
+import { initUI, updateSidebarUI } from './uiController.js';
 
+document.addEventListener('DOMContentLoaded', async () => {
+    
+    // The central render loop
+    function onStateChange() {
+        redrawMap3D();
+        updateSidebarUI();
+    }
+
+    // Initialize the separated components
+    initMap(onStateChange);
+    initUI(onStateChange);
+    
+    // Initial UI render
+    onStateChange();
+});
