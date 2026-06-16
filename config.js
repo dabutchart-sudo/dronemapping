bebe19acce4070e1bd82a13f198afdb91a4bb5ad
dabutchart-sudo <dabutchart@googlemail.com>
2026-06-16 20:04:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,2 @@
 // config.js
-const CONFIG = {
-    // Replace the string below with your actual Cesium ion access token
-    CESIUM_ION_TOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzYzY3ODdhNi1hOTY2LTQ1YmEtYjgzNi03ZTc0YzI1MzkwODAiLCJpZCI6NDM2MjY3LCJzdWIiOiJiZDUyOCIsImlzcyI6Imh0dHBzOi8vYXBpLmNlc2l1bS5jb20iLCJhdWQiOiJVbnRpdGxlZCIsImlhdCI6MTc4MTU0MDI3MX0.uw8qrIh2nHrCFEvssOzlEsXkUh5mrCo5iaLxEO1qITM'
-};
+export const CESIUM_ION_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzYzY3ODdhNi1hOTY2LTQ1YmEtYjgzNi03ZTc0YzI1MzkwODAiLCJpZCI6NDM2MjY3LCJzdWIiOiJiZDUyOCIsImlzcyI6Imh0dHBzOi8vYXBpLmNlc2l1bS5jb20iLCJhdWQiOiJVbnRpdGxlZCIsImlhdCI6MTc4MTU0MDI3MX0.uw8qrIh2nHrCFEvssOzlEsXkUh5mrCo5iaLxEO1qITM';
